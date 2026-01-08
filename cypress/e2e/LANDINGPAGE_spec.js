3d069814d00c728b41fbb803db30332e10db3101
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-01-07 19:02:36 -0800

    update project

--- cypress/e2e/LANDINGPAGE_spec.js
+++ cypress/e2e/LANDINGPAGE_spec.js
@@ -20,16 +20,6 @@ describe('landing page spec', () => {
       .should('have.property', 'status', 200);
   })
 
-  it.skip('opens on landing page', () => {
-
-    // Injecting a simple XSS payload
-    cy.get('textarea[name="comment"]').type('<script>alert("XSS Attack!");</script>');
-    cy.get('button[type="submit"]').click();
-
-    // Assert that the page does not show the script as a result
-    cy.get('body').should('not.contain', 'XSS Attack!');
-
-  })
   it('basic cybersecurity headers', () => {
     cy.checkHeaders('/#/');
 
