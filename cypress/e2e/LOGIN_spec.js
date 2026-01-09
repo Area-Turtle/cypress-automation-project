90330d719b1dcaae8cb8ac35b93bb05aab5245fd
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-01-09 01:59:08 -0800

    update login tests

--- cypress/e2e/LOGIN_spec.js
+++ cypress/e2e/LOGIN_spec.js
@@ -1,30 +1,62 @@
+const user = {
+    email: Cypress.env('userEmail'),
+    password: Cypress.env('userPassword')
+}
+const user2 = {
+    email: Cypress.env('userEmail'),
+    password: 'WrongPassword!'
+}
+
 describe('landing page spec', () => {
-    it('opens on login page', () => {
-        cy.request(Cypress.env('baseUrl') + '#/login')
-            .should('have.property', 'status', 200);
-    })
+    // it('opens on login page', () => {
+    //     cy.visit(Cypress.env('baseUrl') + '#/login')
+    //     cy.request(Cypress.env('baseUrl') + '#/login')
+    //         .should('have.property', 'status', 200);
+    // })
 
-    it('create new user', () => {
-        cy.loginNewUser()
-    })
+    // it('create new user', () => {
+    //     cy.login({
+    //         email: Cypress.env('userEmail'),
+    //         password: Cypress.env('userPassword')
+    //     },
+    //     { create: true});
+    // })
 
-    it('login with user', () => {
-        cy.loginExistUser()
-    })
-    it('login with invalid', () => {
+    // it('login with user', () => {
+    //     cy.login({
+    //         email: Cypress.env('userEmail'),
+    //         password: Cypress.env('userPassword')
+    //     },
+    //     { create: false});
 
-    })
+    // });
     it('login/logout', () => {
-  
+        cy.login({
+            email: Cypress.env('userEmail'),
+            password: Cypress.env('userPassword')
+        },{ create: false});
+        cy.logoutUI()
+        cy.get('#navbarAccount > .mat-mdc-button-touch-target').click()
     })
+    // it('login with invalid', () => {
+    //             cy.login({
+    //         email: Cypress.env('userEmail'),
+    //         password: user2.password
+    //     },
+    //     { create: false});
+    //     cy.get('.error').should('be.visible');
 
-    it('session persistence', () => {
+    // })
 
-    })
-    it.skip('basic cybersecurity headers', () => {
-        cy.visit(Cypress.env('baseUrl') + '#/login')
-        cy.checkHeaders('/#/login');
 
-    })
+    // it('session persistence', () => {
+    //     cy.loginSession(user)
+
+    // })
+    // it.skip('basic cybersecurity headers', () => {
+    //     cy.visit(Cypress.env('baseUrl') + '#/login')
+    //     cy.checkHeaders('/#/login');
+
+    // })
 
 })
