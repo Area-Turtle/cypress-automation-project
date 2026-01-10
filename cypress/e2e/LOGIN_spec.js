c6f296ea36ce5eb308256afacf3a4b9eb8a1d072
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-01-09 20:42:16 -0800

    update login again

--- cypress/e2e/LOGIN_spec.js
+++ cypress/e2e/LOGIN_spec.js
@@ -14,44 +14,44 @@ describe('landing page spec', () => {
             .should('have.property', 'status', 200);
     })
 
-    // it('create new user', () => {
-    //     cy.login({
-    //         email: Cypress.env('userEmail'),
-    //         password: Cypress.env('userPassword')
-    //     },
-    //         { create: true });
-    // })
+    it('create new user', () => {
+        cy.login({
+            email: Cypress.env('userEmail'),
+            password: Cypress.env('userPassword')
+        },
+            { create: true });
+    })
 
-    // it('login with user', () => {
-    //     cy.login({
-    //         email: Cypress.env('userEmail'),
-    //         password: Cypress.env('userPassword')
-    //     },
-    //         { create: false });
+    it('login with user', () => {
+        cy.login({
+            email: Cypress.env('userEmail'),
+            password: Cypress.env('userPassword')
+        },
+            { create: false });
 
-    // });
-    // it('login/logout', () => {
-    //     cy.login({
-    //         email: Cypress.env('userEmail'),
-    //         password: Cypress.env('userPassword')
-    //     },
-    //         { create: false });
-    //     cy.logout()
-    // })
-    // it('login with invalid', () => {
-    //     cy.login({
-    //         email: Cypress.env('userEmail'),
-    //         password: user2.password
-    //     },
-    //         { create: false });
-    //     cy.get('.error').should('be.visible');
+    });
+    it('login/logout', () => {
+        cy.login({
+            email: Cypress.env('userEmail'),
+            password: Cypress.env('userPassword')
+        },
+            { create: false });
+        cy.logout()
+    })
+    it('login with invalid', () => {
+        cy.login({
+            email: Cypress.env('userEmail'),
+            password: user2.password
+        },
+            { create: false });
+        cy.get('.error').should('be.visible');
 
-    // })
+    })
 
-    // it('session persistence', () => {
-    //     cy.loginSession(user)
+    it('session persistence', () => {
+        cy.loginSession(user)
 
-    // })
+    })
     it.skip('basic cybersecurity headers', () => {
         cy.visit(Cypress.env('baseUrl') + '#/login')
         cy.checkHeaders('/#/login');
