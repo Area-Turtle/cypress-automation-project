8fb7be2e2a652d43a7add2fed71edb9ad1b338a2
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-01-10 00:05:34 -0800

    remove test function

--- cypress/e2e/LOGIN_spec.js
+++ cypress/e2e/LOGIN_spec.js
@@ -6,36 +6,7 @@ const user2 = {
     email: Cypress.env('userEmail'),
     password: 'WrongPassword!'
 }
-function loginTemp(user, create) {
-    cy.visit(Cypress.env('baseUrl') + '#/login')
-    if (create) {
-        // Register new user
-        cy.visit('/undefined#/register');
-        cy.get('#emailControl')
-            .should('be.visible').type(user.email);
-        cy.get('#passwordControl')
-            .should('be.visible').type(user.password);
-        cy.get('#repeatPasswordControl')
-            .should('be.visible').type(user.password);
 
-        cy.get('.mat-mdc-select-placeholder')
-            .should('be.visible').click({ force: true });
-        cy.get('#mat-option-0')
-            .should('be.visible').click({ force: true });
-        cy.get('#securityAnswerControl')
-            .should('be.visible').type('abc');
-        cy.get('#registerButton')
-            .should('be.visible').click({ force: true });
-    } else {
-        // Login existing user
-        cy.get('[name="email"]')
-            .should('be.visible').type(user.email);
-        cy.get('[name="password"]')
-            .should('be.visible').type(user.password);
-        cy.get('#loginButton')
-            .should('be.visible').click({ force: true });
-    }
-}
 
 
 describe('landing page spec', () => {
@@ -46,7 +17,7 @@ describe('landing page spec', () => {
     })
 
     it('create new user', () => {
-        loginTemp({
+        cy.login({
             email: Cypress.env('userEmail'),
             password: Cypress.env('userPassword')
         },
@@ -76,7 +47,7 @@ describe('landing page spec', () => {
             password: user2.password
         },
             { create: false });
-        //cy.get('.error').should('be.visible');
+        cy.get('.error').should('be.visible');
 
     })
 
